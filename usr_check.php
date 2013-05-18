807c7d14819d96edb96d18ae1448ae9a2fb2b83d
gtadudeps <emailpiyush@icloud.com>
2013-05-19 02:13:09 +0530

    Update usr_check.php

--- usr_check.php
+++ usr_check.php
@@ -1,6 +1,6 @@
 <?php
-include 'global.php';
-$username=mysql_real_escape_string($_POST['username']);
+include 'global.php'; //global.php connects to the database
+$username=mysql_real_escape_string($_POST['username']); 
 $check = mysql_query("SELECT * FROM users WHERE user_name='$username'");
     if (mysql_num_rows($check)>=1){
 	   $str='<strong><span style="color:red;">Username already taken!</span></strong>';
